f38435bbc5ab3dbf7c4aaa897bda930341c122a2
Lakshmi-Kameswari <238x1a4264@khitguntur.ac.in>
2026-03-11 18:54:59 +0530

    Update server.py

--- chat-application.py/server.py
+++ chat-application.py/server.py
@@ -1,84 +1,79 @@
 import socket
 import threading
-from datetime import datetime
-
-HOST = "127.0.0.1"
-PORT = 5555
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind((HOST, PORT))
-server.listen()
+from config import HOST, PORT, BUFFER_SIZE
 
 clients = []
 nicknames = []
 
-print("=" * 50)
-print("  🚀 Chat Server Started")
-print(f"  Listening on {HOST}:{PORT}")
-print("=" * 50)
 
-
-def broadcast(message, sender_client=None):
-    """Send a message to all connected clients."""
+def broadcast(message):
     for client in clients:
         try:
             client.send(message)
         except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            nicknames.remove(nickname)
+            pass
+
+
+def update_users():
+    names = ",".join(nicknames)
+    msg = f"USERS:{names}".encode("utf-8")
+    broadcast(msg)
 
 
 def handle(client):
-    """Handle messages from a connected client."""
     while True:
         try:
-            message = client.recv(2048)
-            if message:
-                broadcast(message, client)
-                print(f"[{datetime.now().strftime('%H:%M:%S')}] {message.decode('utf-8')}")
+            message = client.recv(BUFFER_SIZE)
+
+            if not message:
+                raise Exception("Disconnected")
+
+            broadcast(message)
+
         except:
-            if client in clients:
-                index = clients.index(client)
-                clients.remove(client)
-                client.close()
-                nickname = nicknames[index]
-                nicknames.remove(nickname)
-                leave_msg = f"SYSTEM:{nickname} has left the chat."
-                broadcast(leave_msg.encode("utf-8"))
-                print(f"[-] {nickname} disconnected.")
+            index = clients.index(client)
+            clients.remove(client)
+
+            nickname = nicknames[index]
+            nicknames.remove(nickname)
+
+            broadcast(f"SYSTEM:{nickname} left the chat".encode("utf-8"))
+            update_users()
+
+            client.close()
             break
 
 
 def receive():
-    """Accept incoming client connections."""
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen()
+
+    print(f"Server running on {HOST}:{PORT}")
+
     while True:
         client, address = server.accept()
-        print(f"[+] New connection from {address}")
+        print("Connected with", address)
 
         client.send("NICK".encode("utf-8"))
-        nickname = client.recv(1024).decode("utf-8")
+        nickname = client.recv(BUFFER_SIZE).decode("utf-8")
+
+        if nickname in nicknames:
+            client.send("SYSTEM:Nickname already taken".encode("utf-8"))
+            client.close()
+            continue
 
         nicknames.append(nickname)
         clients.append(client)
 
-        print(f"[+] Nickname registered: {nickname}")
+        print("Nickname:", nickname)
 
-        # Notify everyone
-        join_msg = f"SYSTEM:{nickname} has joined the chat! 👋"
-        broadcast(join_msg.encode("utf-8"))
+        broadcast(f"SYSTEM:{nickname} joined the chat".encode("utf-8"))
+        update_users()
 
-        # Send online user list to the new client
-        user_list = ",".join(nicknames)
-        client.send(f"USERS:{user_list}".encode("utf-8"))
-
-        # Start handling thread
         thread = threading.Thread(target=handle, args=(client,))
-        thread.daemon = True
         thread.start()
 
 
-receive()
+if __name__ == "__main__":
+    receive()
